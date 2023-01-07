7e23775a21e511e9113197d59e3b5768654fee19
Babban <chaitanya.wankhede.ai@ghrce.raisoni.net>
2023-01-07 19:58:24 +0530

    added sign up

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:paws/auth/auth_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,8 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       //added new file
-      home: Text('Flutter Demo Home Page'),
+      //home: Text('Flutter Demo Home Page'),
+      home: AuthPage(),
     );
   }
 }
