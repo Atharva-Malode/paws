f8984ed3ee1991bce6919d68462984edce0dd722
Atharva Malode <Atharvamalode21@gmail.com>
2023-01-08 01:10:09 +0530

    changed UI of LoginScreen

--- lib/pages/log_in_screen.dart
+++ lib/pages/log_in_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -11,27 +14,107 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-          body:
-              //Logo
-              Padding(
+        child: Scaffold(
+      body:
+          //Logo
+          Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Icon(Icons.android, size: 100),
+              SizedBox(height: 75),
               //Text
               Text(
-                'Hey there, welcome back',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 45),
+                'Hello Again!',
+                style: GoogleFonts.bebasNeue(fontSize: 40),
               ),
+              SizedBox(height: 9),
+              Text(
+                'Welcome back,you have been missed!',
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(height: 50),
               //Email
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.white),
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Email',
+                        contentPadding: EdgeInsets.all(10.0)),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
               //Password
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.white),
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Password',
+                        contentPadding: EdgeInsets.all(10.0)),
+                  ),
+                ),
+              ),
               //Login Button
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 8),
               //dont have an account? Sign up
+
+              //added row widget for gesture
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Not a member?',
+                      style: TextStyle(
+                          // color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15)),
+                  Text('Register Now',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15))
+                ],
+              ),
             ],
           ),
         ),
-      )),
-    );
+      ),
+    ));
   }
 }
